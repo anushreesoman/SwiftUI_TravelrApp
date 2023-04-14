c623568f1ef2da05ea70febc00eb2476d9e9d417
Anushree Soman <anuso.deepak@gmail.com>
2023-04-14 11:14:02 -0400

    Initial Commit

---
+++ Travelr/TravelrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelrApp.swift
+//  Travelr
+//
+//  Created by Anushree Soman on 4/14/23.
+//
+
+import SwiftUI
+
+@main
+struct TravelrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
